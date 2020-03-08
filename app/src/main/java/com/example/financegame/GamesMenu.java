1aca46815fcefde49d087b8b56da3f186b487f1d
ahan <annahan17@gmail.com>
2020-03-07 20:51:44 -0800

    games menu added

---
+++ app/src/main/java/com/example/financegame/GamesMenu.java
@@ -0,0 +1,12 @@
+package com.example.financegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GamesMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_games_menu)
