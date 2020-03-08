9d3a3338cca8e59253a1d70aea4474e00627e354
ahan <annahan17@gmail.com>
2020-03-07 21:57:44 -0800

    market created

---
+++ app/src/main/java/com/example/financegame/Game4.java
@@ -0,0 +1,14 @@
+package com.example.financegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Game4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_4);
+    }
+}
--- app/src/main/java/com/example/financegame/MainActivity.java
+++ app/src/main/java/com/example/financegame/MainActivity.java
@@ -2,13 +2,71 @@ package com.example.financegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button gamesMenuButton, homeButton, marketButton, myStocksButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeButton = (Button) findViewById(R.id.button);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHome();
+            }
+        });
+
+        myStocksButton = (Button) findViewById(R.id.button);
+        myStocksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMyStocks();
+            }
+        });
+
+        marketButton = (Button) findViewById(R.id.button);
+        marketButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMarket();
+            }
+        });
+
+        gamesMenuButton = (Button) findViewById(R.id.button);
+        gamesMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGamesMenu();
+            }
+        });
+    }
+
+    public void openHome() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void openMyStocks() {
+        Intent intent = new Intent(this, GamesMenu.class);
+        startActivity(intent);
+    }
+
+    public void openMarket() {
+        Intent intent = new Intent(this, GamesMenu.class);
+        startActivity(intent);
+    }
+
+    public void openGamesMenu() {
+        Intent intent = new Intent(this, GamesMenu.class);
+        startActivity(intent);
     }
 }
+
+
