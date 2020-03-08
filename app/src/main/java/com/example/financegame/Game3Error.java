e8f630cedc0994c17c0e819205c81567f7665b21
ahan <annahan17@gmail.com>
2020-03-08 11:58:43 -0700

    added game 3 intial

---
+++ app/src/main/java/com/example/financegame/Game3Error.java
@@ -0,0 +1,14 @@
+package com.example.financegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Game3Error extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game3_error);
+    }
+}
---
+++ app/src/main/java/com/example/financegame/Game3play.java
@@ -0,0 +1,14 @@
+package com.example.financegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Game3play extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game3play);
+    }
+}
